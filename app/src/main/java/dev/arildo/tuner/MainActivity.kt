a34906a0017fd84dbbe3ce36e3b44ab5b6a10d96
Arildo Borges Jr <sitesabj@gmail.com>
2021-11-15 16:05:23 -0300

    add missing compose libs and minor refactor

--- app/src/main/java/dev/arildo/tuner/MainActivity.kt
+++ app/src/main/java/dev/arildo/tuner/MainActivity.kt
@@ -1,18 +1,74 @@
 package dev.arildo.tuner
 
-import android.app.Activity
 import android.os.Bundle
-import dev.arildo.tuner.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.ExperimentalWearMaterialApi
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import dev.arildo.tuner.theme.OutOfTune
+import dev.arildo.tuner.theme.Tuned
 
-class MainActivity : Activity() {
-
-    private lateinit var binding: ActivityMainBinding
+@ExperimentalWearMaterialApi
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContent {
+            MaterialTheme {
+                Scaffold(
+                    timeText = {
+                        TimeText()
+                    }
+                ) {
+                    Layout(Tuned)
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun Layout(color: Color) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "B", modifier = Modifier.wrapContentSize(),
+            textAlign = TextAlign.Center,
+            fontSize = 84.sp
+        )
+    }
+}
+
+@Preview(widthDp = 200, heightDp = 200)
+@Composable
+fun TunedView() {
+    Layout(Tuned)
+}
+
+@Preview(widthDp = 200, heightDp = 200)
+@Composable
+fun OutOfTuneView() {
+    Layout(OutOfTune)
 }
---
+++ app/src/main/java/dev/arildo/tuner/theme/Color.kt
@@ -0,0 +1,10 @@
+package dev.arildo.tuner.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF5e6ff7)
+val PrimaryDark = Color(0xFF111FF7)
+val Accent = Color(0xFF03DAC5)
+
+val Tuned = Color(0xFF574B72E)
+val OutOfTune = Color(0xFFD21F3C)
